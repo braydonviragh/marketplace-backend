869200fbb03512f8e155d1e69374e7770b6b1a2a
Braydon Viragh <braydonviragh@Braydons-MacBook-Pro.local>
2025-05-12 15:45:19 -0400

    aws s3 config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Services\GeocodingService;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Switch to S3 filesystem when running in Railway environment
+        if (env('RAILWAY_ENVIRONMENT')) {
+            Config::set('filesystems.default', env('RAILWAY_FILESYSTEM_DISK', 's3'));
+            
+            // Update the MediaResource URL generation to use absolute URLs from S3
+            \App\Models\Media::resolveUrlUsing(function ($media) {
+                if ($media->disk === 's3') {
+                    return Config::get('filesystems.disks.s3.url') . '/' . $media->path;
+                }
+                return url('/storage/' . $media->path);
+            });
+        }
     }
 }
