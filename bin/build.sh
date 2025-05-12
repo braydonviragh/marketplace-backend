2aed21887482f8c6e9b24dfafbfe873094a25dd2
Braydon Viragh <braydonviragh@Braydons-MacBook-Pro.local>
2025-05-12 09:02:13 -0400

    adding storage link

--- bin/build.sh
+++ bin/build.sh
@@ -87,6 +87,6 @@ echo "⚙️ Optimizing application for production..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
+php artisan storage:link
 echo "✅ Build process completed successfully!"
 exit 0 
