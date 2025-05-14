2e397280d0dc0757010b2ef2041ed545135f4c94
Braydon Viragh <braydonviragh@Braydons-MacBook-Pro.local>
2025-05-14 11:46:19 -0400

    new frontend build

---
+++ public/assets/global-components.2530514b.js
@@ -0,0 +1,1 @@
+import{b as t,Q as e}from"./index.c533f99f.js";var n=t(({app:o})=>{o.component("q-btn",{extends:e,props:{noCaps:{type:Boolean,default:!0}}})});export{n as default};
---
+++ public/assets/use-quasar.22bfa1f7.js
@@ -0,0 +1,1 @@
+import{af as a,bb as r}from"./index.c533f99f.js";function u(){return a(r)}export{u};
